c723c0427866e5f8e53f963aac853d0398ef44ba
Frankie <frankie710@gmail.com>
2020-02-19 16:28:22 -0500

    pybank

---
+++ PyBank/main.py
@@ -0,0 +1,57 @@
+import os
+import csv
+total = 0
+monthsarray = []
+profit = []
+diff = []
+
+budget_csv = os.path.join("Resources", "budget_data.csv")
+
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        monthsarray.append(row[0])
+        total += int(row[1])
+        profit.append(int(row[1]))
+    #print(len(profit))
+    for x in range(len(profit)):
+        if x + 1 < len(monthsarray):
+            #print(profit[x])
+            difference = profit[x + 1] - profit[x]
+            diff.append(difference) 
+            #print(profit[x])
+        #elif x > months:
+            #print("Done")
+        #diff = profit[x+1] - profit[x]
+        #print(diff)
+
+increase = max(diff)
+increasepos = diff.index(max(diff))
+decrease = min(diff)
+decreasepos = diff.index(min(diff))
+months = len(monthsarray)
+
+#print(len(monthsarray))
+#print(total)
+averagechange = sum(diff) / len(diff)
+#print(averagechange)
+#print(increase)
+#print(decrease)
+#print(monthsarray[increasepos+1])
+#print(monthsarray[decreasepos+1])
+
+
+
+output_path = os.path.join("output.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["-------------------------------"])
+    csvwriter.writerows([[f"Total Months:  {months}"],
+    [f"Total: {total}"],
+    [f"Average Change: ${averagechange}"],
+    [f"Greatest Increase in Profits: {increasepos} ({increase})"],
+    [f"Greatest Decrease in Profits: {decreasepos} ({decrease})"]])
+
+
